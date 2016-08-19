7d5656fe6d10ddf7981b94c62c2e3c494d77c244
Antonin Cezard <anc@ecedi.fr>
2016-08-19 20:32:27 +0200

    Beautify backgrounds

--- public/main.js
+++ public/main.js
@@ -114,61 +114,77 @@ exports.draw = function() {
 },{"./canvasconf":1,"./input":4}],6:[function(require,module,exports){
 var conf = require('./canvasconf');
 
-var starfield,
-    starfield2,
-    wave,
-    starX = 0,
-    starY = 0,
-    starY2 = -900,
-    star2Y = 0
-    star2Y2 = -593,
-    waveX = 0,
-    waveY = 0;
+var bigstars = {
+  x: 0,
+  y: 0,
+  y2: 0,
+  src: 'assets/images/background.png',
+  h: 0,
+  speed: 3
+}
 
-starfield = new Image();
-starfield.src = 'assets/images/background.png';
-starfield2 = new Image();
-starfield2.src = 'assets/images/bg2.png';
+foreground = new Image();
+foreground.src = bigstars.src;
+foreground.onload = function() {
+  bigstars.h = foreground.naturalHeight;
+  bigstars.y2 = - bigstars.h;
+}
+
+var smallstars = {
+  x: 0,
+  y: 0,
+  y2: 0,
+  src: 'assets/images/bg2.png',
+  h: 0,
+  speed: 2
+}
+
+middleground = new Image();
+middleground.src = smallstars.src;
+middleground.onload = function() {
+  smallstars.h = middleground.naturalHeight;
+  smallstars.y2 = - smallstars.h;
+}
 
 var wave = {
   y: 0,
   x: 0,
   src: 'assets/images/wave.png',
-  height: 491
+  h: 0,
+  speed: 1
 }
+
 background = new Image();
 background.src = wave.src;
+background.onload = function() {
+  wave.h = background.naturalHeight;
+}
 
 exports.update = function() {
-  wave.y += 0.25;
-  if (wave.y > 1000) wave.y = - wave.height;
+  // Background
+  if (wave.y > 1000) wave.y = - wave.h; // 1000 is arbitrary
+  wave.y += wave.speed;
 
-  if (star2Y > 593) {
-    star2Y = -592;
-  }
-  if (star2Y2 > 593) {
-    star2Y2 = -592;
-  }
-  star2Y += 0.5;
-  star2Y2 += 0.5;
+  // Middleground
+  if (smallstars.y > smallstars.h) smallstars.y = (-smallstars.h) - smallstars.speed;
+  if (smallstars.y2 > smallstars.h) smallstars.y2 = (-smallstars.h) - smallstars.speed;
+  smallstars.y += smallstars.speed;
+  smallstars.y2 += smallstars.speed;
 
-  if (starY > 900) {
-    starY = -899;
-  }
-  if (starY2 > 900) {
-    starY2 = -899;
-  }
-  starY += 1;
-  starY2 += 1;
+  // Foreground
+  if (bigstars.y > bigstars.h) bigstars.y = (-bigstars.h) - bigstars.speed;
+  if (bigstars.y2 > bigstars.h) bigstars.y2 = (-bigstars.h) - bigstars.speed;
+  bigstars.y += bigstars.speed;
+  bigstars.y2 += bigstars.speed;
 };
 
 exports.draw = function () {
   conf.ctx.drawImage(background, wave.x, wave.y);
 
-  conf.ctx.drawImage(starfield2, starX, star2Y);
-  conf.ctx.drawImage(starfield2, starX, star2Y2);
+  conf.ctx.drawImage(middleground, smallstars.x, smallstars.y);
+  conf.ctx.drawImage(middleground, smallstars.x, smallstars.y2);
 
-  conf.ctx.drawImage(starfield, starX, starY);
-  conf.ctx.drawImage(starfield, starX, starY2);
+  conf.ctx.drawImage(foreground, bigstars.x, bigstars.y);
+  conf.ctx.drawImage(foreground, bigstars.x, bigstars.y2);
 };
 },{"./canvasconf":1}]},{},[3]);
